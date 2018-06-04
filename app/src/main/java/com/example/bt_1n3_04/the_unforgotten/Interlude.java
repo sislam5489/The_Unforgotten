212b0afb72545d1d1b0ff2d26970066521efa49e
BT_1N3_04@BKTECH.LOCAL <BT_1N3_04@BKTECH.LOCAL>
2018-06-04 13:01:08 -0400

    Initial commit

---
+++ app/src/main/java/com/example/bt_1n3_04/the_unforgotten/Interlude.java
@@ -0,0 +1,15 @@
+package com.example.bt_1n3_04.the_unforgotten;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Interlude extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interlude);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/bt_1n3_04/the_unforgotten/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.bt_1n3_04.the_unforgotten;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Go to another activity
+        Button btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Interlude.class));
+            }
+        });
+    }
+}
